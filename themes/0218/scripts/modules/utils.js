aef72dfba9245ff8d10a09ef66f87f6b6661aa74
hiro <hiro0218@gmail.com>
2020-09-01 11:35:29 +0900

    fix: replace cheerio

--- themes/0218/scripts/modules/utils.js
+++ themes/0218/scripts/modules/utils.js
@@ -1,39 +1,42 @@
+const cheerio = require('cheerio');
+
 module.exports = {
   /**
    * h2の内容をを取得して中身を取り出す
    */
   getHeadings(content) {
-    const headings = content.match(/<h2>(.*?)<\/h2>/gim);
+    const $ = cheerio.load(content);
+    const $h2 = $('h2');
+    const headings = [];
 
-    if (!headings) return '';
+    $h2.each(function (i) {
+      // 5つだけ抽出
+      if (i < 5) {
+        headings[i] = $(this).text();
+      }
+    });
 
-    const excerpt = headings
-      .map((a) => {
-        return a.replace(/<(|\/)h2>/gim, '');
-      })
-      .join(' / ');
-
-    return excerpt;
+    return headings.join(' / ');
   },
 
   /**
    * 記事内容から画像URLを取得する
    */
   getThumbnail(content) {
-    // img要素のデータを取得
-    const imgData = content.match(/img.*?src\=([^\s]*)\s/);
-    if (imgData) {
-      // src属性の中身を取得
-      const srcWithQuotes = imgData[1];
-      const src = srcWithQuotes.substring(1, srcWithQuotes.length - 1);
+    const $ = cheerio.load(content);
+    const $img = $('img[src]');
+    const regex = new RegExp('^(https)://', 'i');
+    let thumbnail = '';
+
+    $img.each(function (i) {
+      const src = $(this).attr('src');
 
       // 妥当性をチェック
-      const regex = new RegExp('^(https)://', 'i');
       if (regex.test(src)) {
-        return src;
+        thumbnail = src;
       }
-    }
+    });
 
-    return '';
+    return thumbnail;
   },
 };
