db71d18201e86c9e802f771b1a3d982e4038d7af
hiro <hiro0218@gmail.com>
2020-12-12 00:14:22 +0900

    fix: categories option

--- themes/0218/scripts/categories.js
+++ themes/0218/scripts/categories.js
@@ -1,18 +1,13 @@
 hexo.extend.generator.register('categories', (locals) => {
-  const categories_data = locals.data.categories;
   const categories = locals.categories;
   const data = [];
 
   categories.forEach((category) => {
-    const [category_data] = categories_data.filter((data) => data.name === category.name);
-
     data.push({
       name: category.name,
       slug: category.slug,
       path: encodeURI(category.path),
       count: category.length,
-      pickup: !!category_data,
-      logo: category_data ? category_data.logo : '',
     });
   });
 
