1179bf3f34bd0b6c54f1f39e2e46186e8d63f25d
hiro <hiro0218@gmail.com>
2020-12-16 01:25:05 +0900

    add: recent/updates

--- themes/0218/scripts/archives.js
+++ themes/0218/scripts/archives.js
@@ -1,8 +1,7 @@
 hexo.extend.generator.register('archives', (locals) => {
-  const posts = locals.posts;
   const data = [];
 
-  posts.forEach((post) => {
+  locals.posts.forEach((post) => {
     data.push({
       title: post.title,
       date: post.date,
@@ -18,5 +17,57 @@ hexo.extend.generator.register('archives', (locals) => {
   return {
     path: 'archives.json',
     data: JSON.stringify(data),
+  };
+});
+
+hexo.extend.generator.register('recent_posts', (locals) => {
+  const data = [];
+
+  locals.posts.forEach((post) => {
+    data.push({
+      title: post.title,
+      date: post.date,
+      updated: post.updated,
+      path: post.path,
+    });
+  });
+
+  // sort: 更新日付順
+  data.sort((a, b) => {
+    return a.date < b.date ? 1 : -1;
+  });
+
+  // 5件絞り込み
+  const posts = data.filter((_, i) => i < 5);
+
+  return {
+    path: 'recent_posts.json',
+    data: JSON.stringify(posts),
+  };
+});
+
+hexo.extend.generator.register('updates_posts', (locals) => {
+  const data = [];
+
+  locals.posts.forEach((post) => {
+    data.push({
+      title: post.title,
+      date: post.date,
+      updated: post.updated,
+      path: post.path,
+    });
+  });
+
+  // sort: 更新日付順
+  data.sort((a, b) => {
+    return a.updated < b.updated ? 1 : -1;
+  });
+
+  // 5件絞り込み
+  const posts = data.filter((_, i) => i < 5);
+
+  return {
+    path: 'updates_posts.json',
+    data: JSON.stringify(posts),
   };
 });
