613f94d1cb9860c2e26300789c2e9d2196693e0c
hiro <hiro0218@gmail.com>
2020-12-31 01:38:49 +0900

    fix: fs-extra関数へ変更

--- ogp.js
+++ ogp.js
@@ -1,24 +1,11 @@
 const fs = require('fs-extra');
-const path = require('path');
 const generateOgpImage = require('./themes/0218/scripts/modules/ogp');
 
 const ogpImages = [];
 const ogpDirectoryPath = './public/images/ogp';
 
-const makeOgpDir = () => {
-  try {
-    // 対象のディレクトリが存在するか
-    fs.statSync(ogpDirectoryPath);
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      // ディレクトリを作成
-      fs.mkdirsSync(path.resolve(ogpDirectoryPath));
-    }
-  }
-};
-
 (async () => {
-  makeOgpDir();
+  fs.ensureDirSync(ogpDirectoryPath);
 
   fs.readJson('./public/archives.json')
     .then((archives) => {
